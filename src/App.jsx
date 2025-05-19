eb8d8803968e59939a99356d1d366c55cb28f075
DevySrabony <srabonydevy@gmail.com>
2025-05-19 21:48:53 +0600

    setUp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-4xl text-center mx-auto text-orange-400 font'>Meet My Roomie</h1>
+    </>
+  )
+}
+
+export default App
